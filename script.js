3a4430da081ffb3ea20ce33449f3eb8b124641fe
Marcos Vieira <marcosvieira482@gmail.com>
2024-04-29 19:27:27 -0300

    feat: add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const addQuestionCard = document.querySelector("add-card-modal");
+const cardButton = document.getElementById("save-btn");
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const errosMessage = document.getElementById("error");
+const addQuestion = document.getElementById("add-card");
+const closeBtn = document.getElementById("close-btn");
+
